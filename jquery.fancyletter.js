4384b2991a0ccd47cb25705a9470d16e037a7a64
Craig Erskine <craig@craigerskine.com>
2019-02-27 15:20:32 -0600

    Update jquery.fancyletter.js

--- jquery.fancyletter.js
+++ jquery.fancyletter.js
@@ -1,24 +1,35 @@
-// fancyLetter 1.0.0
-(function($){
-  $.fn.fancyletter = function(options){
-    return this.each(function(){
-      var opts = $.extend({}, $.fn.fancyletter.defaults, options || {}, $.metadata ? $cont.metadata() : $.meta ? $cont.data() : {});
-      var node = this;
-      while (node.childNodes.length){
-        node = node.firstChild
-      }
-      var text = node.nodeValue;
-      var firstLetter = text.slice(0, 1);
-      var re = new RegExp(opts.characters);
-      if (re.test(firstLetter)){
-        node.nodeValue = text.slice(1);
-        $(['<span class="', opts.commonClass, ' ', opts.ltrClassPrefix, firstLetter.toLowerCase(), '">', firstLetter, '</span>'].join('')).prependTo(this)
-      }
-    })
-  };
-  $.fn.fancyletter.defaults = {
+;(function($, window, document, undefined){
+  var pluginName = 'fancyletter',
+  defaults = {
     commonClass: 'ltr',
-    ltrClassPrefix: 'ltr-',
+    prefixClass: 'ltr-',
     characters: '[a-zA-Z]'
+  };
+
+  function Plugin(element, options){
+    this.element = element;
+    this.options = $.extend({}, defaults, options);
+    this._defaults = defaults;
+    this._name = pluginName;
+    this.init();
   }
-})(jQuery);
+
+  Plugin.prototype.init = function(){
+    var re = new RegExp(this.options.characters);
+    var firstletter = this.element.firstChild.nodeValue.slice(0, 1);
+    if($(this.element).attr('data-plugin') !== this._name && re.test(firstletter)){
+      this.element.firstChild.nodeValue = this.element.firstChild.nodeValue.slice(1);
+      $(this.element).attr('data-plugin', this._name);
+      $(['<span class="', this.options.commonClass, ' ', this.options.prefixClass, firstletter.toLowerCase(), '" />'].join('')).text(firstletter).prependTo(this.element);
+    }
+  };
+
+  $.fn[pluginName] = function(options){
+    return this.each(function(){
+      if(!$.data(this, 'plugin_' + pluginName)){
+        $.data(this, 'plugin_' + pluginName, 
+        new Plugin(this, options));
+      }
+    });
+  }
+})(jQuery, window, document);
